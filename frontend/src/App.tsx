708a8955c3abeaedd41d89a1f7113676d5cdaf34
Junta <junta8918@gmail.com>
2021-08-05 11:48:29 +0900

    first commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Symfoni } from "./hardhat/SymfoniContext";
-import { Greeter } from './components/Greeter';
+import React, { useContext, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Symfoni,
+  CurrentAddressContext,
+  GreeterContext,
+} from "./hardhat/SymfoniContext";
+import { Board } from "./components/Board";
+import { Greeter } from "./components/Greeter";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
-        <Symfoni autoInit={true} >
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
+        <Symfoni autoInit={true}>
+          <Board />
+
+          {/*
           <Greeter></Greeter>
+          */}
         </Symfoni>
       </header>
     </div>
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,15 +1,30 @@
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 describe("Token", function () {
   let accounts: Signer[];
+  let greeter;
 
   beforeEach(async function () {
     accounts = await ethers.getSigners();
+    const Greeter = await ethers.getContractFactory("Greeter");
+    greeter = await Greeter.deploy("5");
+    await greeter.deployed();
   });
 
   it("should have accounts", async function () {
     assert(accounts.length > 0, "Account legth should be more then zero");
+  });
+
+  it("should greet equal to 5", async function () {
+    const greet_string = await greeter.greet();
+    expect(greet_string).to.equal("5");
+  });
+
+  it("should setGreeting work correctly", async function () {
+    const greet_setted = await greeter.setGreeting("4");
+    const greet_string = await greeter.greet();
+    expect(greet_string).to.equal("4");
   });
 });
